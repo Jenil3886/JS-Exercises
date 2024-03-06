07cfcab4c458dee482f147cff39631f07bef3482
jenil gajera <gajerajenil08@gmail.com>
2024-03-06 22:31:03 +0530

    first file done

---
+++ first.js
@@ -0,0 +1,13 @@
+// write a JS program to calculate sum of two numbers
+
+function sum(a, b) {
+    return a + b
+}
+
+let result1 = sum(10, 20)
+let result2 = sum(5, 8)
+let result3 = sum(35, 12)
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
